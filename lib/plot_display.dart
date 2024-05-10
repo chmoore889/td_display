f84ced3451d5b3f5f7fd9c39b46b9d084f2c43fe
Christopher Moore <51140505+chmoore889@users.noreply.github.com>
2024-05-10 12:36:28 -0400

    Added y axis control for correlation display

--- lib/plot_display.dart
+++ lib/plot_display.dart
@@ -2,7 +2,7 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:tt_bindings/tt_bindings.dart';
 
-class TPSFDisplay extends StatelessWidget {
+class TPSFDisplay extends StatefulWidget {
   final Stream<(Map<int, int>, Iterable<CorrelationPair>)>? tpsfStream;
   final PostProcessingParams params;
 
@@ -13,9 +13,16 @@ class TPSFDisplay extends StatelessWidget {
   });
 
   @override
+  State<TPSFDisplay> createState() => _TPSFDisplayState();
+}
+
+class _TPSFDisplayState extends State<TPSFDisplay> {
+  double yMin = 0.95, yMax = 1.25;
+
+  @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: tpsfStream,
+      stream: widget.tpsfStream,
       builder: (context, snapshot) {
         if(snapshot.connectionState == ConnectionState.none) {
           return const Center(
@@ -29,7 +36,7 @@ class TPSFDisplay extends StatelessWidget {
         }
 
         final dataList = snapshot.data!.$1.entries.toList();
-        final int countRate = dataList.fold<int>(0, (p, c) => p + c.value) ~/ (params.integrationTimePs / 1e12);
+        final int countRate = dataList.fold<int>(0, (p, c) => p + c.value) ~/ (widget.params.integrationTimePs / 1e12);
 
         dataList.sort((a, b) => a.key.compareTo(b.key));
         return Column(
@@ -52,7 +59,7 @@ class TPSFDisplay extends StatelessWidget {
                   ),
                   AreaSeries<MapEntry<int, int>, int>(
                     animationDuration: 0,
-                    dataSource: dataList.where((element) => params.gatingRange.inRange(element.key)).toList(),
+                    dataSource: dataList.where((element) => widget.params.gatingRange.inRange(element.key)).toList(),
                     xValueMapper: (pair, index) => pair.key,
                     yValueMapper: (pair, index) {
                       return pair.value;
@@ -61,11 +68,42 @@ class TPSFDisplay extends StatelessWidget {
                 ],
               ),
             ),
+            Row(
+              children: [
+                const Text('Y Scale: '),
+                SizedBox(
+                  width: 100,
+                  child: NumberBox(
+                    value: yMin,
+                    onChanged: (e) {
+                      setState(() {
+                        yMin = e ?? yMin;
+                      });
+                    },
+                    mode: SpinButtonPlacementMode.none,
+                    clearButton: false,
+                  ),
+                ),
+                SizedBox(
+                  width: 100,
+                  child: NumberBox(
+                    value: yMax,
+                    onChanged: (e) {
+                      setState(() {
+                        yMax = e ?? yMax;
+                      });
+                    },
+                    mode: SpinButtonPlacementMode.none,
+                    clearButton: false,
+                  ),
+                ),
+              ],
+            ),
             Expanded(
               child: SfCartesianChart(
-                primaryYAxis: const NumericAxis(
-                  // minimum: 0.95,
-                  // maximum: 1.2,
+                primaryYAxis: NumericAxis(
+                  minimum: yMin,
+                  maximum: yMax,
                 ),
                 primaryXAxis: const LogarithmicAxis(
                   minimum: 1e-6,
